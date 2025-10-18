8608de59a6ca048fd3e979658efc1d5e5adad7dc
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-10-19 00:35:44 +0530

    Header done

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,18 +1,63 @@
-import { AppBar, Box, Toolbar, Typography } from '@mui/material'
+import { useState } from "react";
+import { AppBar, Box, Divider, Drawer, IconButton, Toolbar, Typography } from '@mui/material'
 import FastfoodIcon from '@mui/icons-material/Fastfood';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
 import "../../styles/Header.css"
 
 const Header = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  }
+
+  //menu drawer
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center', }}>
+      <Typography color={'goldenrod'} variant='h6' 
+      component='div' 
+      sx={{ flexGrow: 1, my: 2}}>
+        <FastfoodIcon />
+        My Resturant
+      </Typography>
+      <Divider />
+      <ul className="mobile_navigation">
+        <li>
+          <Link to={'/'}>Home</Link>
+        </li>
+        <li>
+          <Link to={'/about'}>About</Link>
+        </li>
+        <li>
+          <Link to={'/contact'}>Contact</Link>
+        </li>
+        <li>
+          <Link to={'/menu'}>Menu</Link>
+        </li>
+      </ul>
+    </Box>
+  )
+
   return (
     <Box>
       <AppBar component={'nav'} sx={{ bgcolor: 'black' }}>
         <Toolbar>
-          <Typography color={'goldenrod'} variant='h6' component='div' sx={{flexGrow: 1}}>
+          <IconButton
+            onClick={handleDrawerToggle}
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <Typography color={'goldenrod'} variant='h6' component='div' sx={{ flexGrow: 1 }}>
             <FastfoodIcon />
             My Resturant
           </Typography>
-          <Box sx={{display: {xs: "none", sm: "block" }}}>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
             <ul className="navigation_menu">
               <li>
                 <Link to={'/'}>Home</Link>
@@ -30,6 +75,18 @@ const Header = () => {
           </Box>
         </Toolbar>
       </AppBar>
+      <Box component="nav">
+        <Drawer variant="temporary" open={mobileOpen}
+          onClose={handleDrawerToggle}
+          sx={{ display: { xs: 'block', sm: 'none' }, "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: "240px",
+          } }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Toolbar />
     </Box>
   )
 }
