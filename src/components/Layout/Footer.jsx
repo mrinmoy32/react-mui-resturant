a4fdf84860691695737c1e13bbbcda08bd95e1c8
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-10-19 03:26:31 +0530

    Footer

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,44 @@
+import { Box, Typography } from '@mui/material'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GithubIcon from '@mui/icons-material/Github';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <Box sx={{textAlign: "center", bgcolor:'#1A1A19', color: 'white', p: 3}}>
+            <Box sx={{my: 3, "& svg": {
+                fontSize: "2.1rem",
+                cursor: "pointer",
+                mr: 2
+            },
+            "& svg:hover": {
+                color: 'goldenrod',
+                transform: 'translateY(-5px)',
+                transition: 'all 400ms',
+            }
+            }}>
+                {/* icons */}
+                <InstagramIcon />
+                <TwitterIcon />
+                <GithubIcon />
+                <YouTubeIcon />
+            </Box>
+            <Typography 
+            variant='h6'
+            sx={{"@media (max-width: 600px)": {
+                fontSize: "0.8rem",
+
+            }}}
+            >
+                All Rights Reserved &copy; Mrinmoy Pal
+            </Typography>
+        </Box>
+    </>
+  )
+}
+
+export default Footer
