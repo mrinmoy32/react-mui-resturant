52c8ff1b22298e60a535419dcf905436c37b0249
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-10-19 05:25:16 +0530

    menu data

--- src/data/data.js
+++ src/data/data.js
@@ -1,13 +1,45 @@
 import Dosa from '../images/dosa.jpg'
 import Chhola from '../images/chhola.jpg'
-import Idli from '../images/Idli.jpg'
-import MasalaDosa from '../images/MasalaDosa.jpg'
+import Idli from '../images/idli.jpg'
+import MasalaDosa from '../images/masala.jpg'
 import Paneer from '../images/paneer.jpg'
 import Gujarati from '../images/gujarati.jpeg'
 
 export const MenuList = [
     {
         name: "Dosa",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, harum."
-    }
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, harum.",
+        image: Dosa,
+        price: 199
+    },
+    {
+        name: "Chhola",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, harum.",
+        image: Chhola,
+        price: 149
+    },
+    {
+        name: "Idli",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, harum.",
+        image: Idli,
+        price: 179
+    },
+    {
+        name: "MasalaDosa",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, harum.",
+        image: MasalaDosa,
+        price: 349
+    },
+    {
+        name: "Paneer",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, harum.",
+        image: Paneer,
+        price: 299
+    },
+    {
+        name: "Gujarati",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, harum.",
+        image: Gujarati,
+        price: 229
+    },
 ]
