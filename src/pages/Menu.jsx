275d82e9c124134149d7dc8ab41643c2fe483912
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-10-19 06:02:26 +0530

    Menu page

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React from 'react';
+import { MenuList } from '../data/data';
+import Layout from '../components/Layout/Layout';
+import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
 
 const Menu = () => {
   return (
-    <Layout>Menu</Layout>
+    <Layout>
+      <Box sx={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
+        {MenuList.map(menu => (
+          <Card sx={{maxWidth: "370px", m:2}}>
+            <CardActionArea>
+              <CardMedia sx={{minHeight: '400px'}} component='img' src={menu.image} alt={menu.name}/>
+              <CardContent>
+                <Typography variant='h5' gutterBottom component='div'>
+                  {menu.name}
+                </Typography>
+                 <Typography variant='nody2' component='div'>
+                  {menu.description}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
+      </Box>
+    </Layout>
   )
 }
 
